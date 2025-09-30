b3b30c8e664f576cb9679e1eecc455a2995b8891
zakiamushtaq5563-oss <zakiamushtaq5563@gmail.com>
2025-09-29 21:55:06 -0700

    abe

---
+++ Operators.py
@@ -0,0 +1,45 @@
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+a=4>2
+print(a)
+b=4<2
+print(b)
+c=4>=4
+print(c)
+d=4>=2
+print(d)
+e=2!=2
+print(e)
+f=2!=2
+print(f)
+g=2==2
+print(g)
+
+marks=int(input("enter your marks:"))
+print(type(marks))
+if marks>90:
+    print("your grade is A")
+elif marks>80:
+    print("your grade is B")
+elif marks>70:
+    print("your grade is C")
+else:
+    print("your grade is D")
+
+marks=int(input("enter overall marks percentage:"))
+cs_marks=int(input("enter overall cs marks percentage:"))
+if marks>=88 or cs_marks>=88:
+    print("you are eligible for admission")
+else:
+    print("you are eligible for admission")
+
+marks=int(input("enter overall marks percentage:"))
+cs_marks=int(input("enter overall cs marks percentage:"))
+if marks>=88 and cs_marks>=88:
+    print("you are eligible for admission")
---
+++ randomization.py
@@ -0,0 +1,12 @@
+import random
+randint=random.randint(1,100)
+print(randint)
+
+randfloat=random.random()
+print(randfloat)
+
+random23=random.randint(1,100)
+ifrandom23==1:
+print("head")
+else:
+      print("tail")
